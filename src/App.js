4dda3cd0995e44e018b51c97e9e4cc8f89951319
Jemila <jemila.abulhawa@gmail.com>
2019-08-21 11:49:19 +0300

    create first store #3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import { createStore } from 'redux'
 
 const App = () => <p>Starter</p>
 
 export default App
+
+const hello = () => ({ welcome: 'hello' })
+const store = createStore(hello)
+console.log(store.getState()) // { welcome: 'hello' }
