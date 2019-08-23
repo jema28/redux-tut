b18715c35bf5e44565049ed1939c173f057d4ad1
Jemila <jemila.abulhawa@gmail.com>
2019-08-23 19:24:24 +0300

    add provider from react-redux and connect store #6

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,23 @@
 /* eslint react/no-did-mount-set-state: 0 */
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import MoviesList from '../MovieList'
 import MovieDetail from '../MovieDetail'
 
+const hello = () => 'hello'
+const store = createStore(hello)
+
 const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={MoviesList} />
-      <Route path="/:id" component={MovieDetail} />
-    </Switch>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />
+        <Route path="/:id" component={MovieDetail} />
+      </Switch>
+    </Router>
+  </Provider>
 )
 
 export default App
