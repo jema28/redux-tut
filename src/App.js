569f4377bb999fa14301efd094c97612e2d0a1d2
Jemila <jemila.abulhawa@gmail.com>
2019-08-23 18:42:25 +0300

    add first reducer and action #4

--- src/App.js
+++ src/App.js
@@ -5,6 +5,26 @@ const App = () => <p>Starter</p>
 
 export default App
 
-const hello = () => ({ welcome: 'hello' })
-const store = createStore(hello)
-console.log(store.getState()) // { welcome: 'hello' }
+const defaultState = {
+  welcome: 'Hi',
+  otherState: 'some stuff'
+}
+
+const greeting = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'GREET_ME':
+      return { ...state, welcome: 'Hello Jem' }
+    case 'GREET_WORLD':
+      return { ...state, welcome: 'Hello world' }
+    default:
+      return state
+  }
+}
+
+const store = createStore(greeting)
+
+store.dispatch({
+  type: 'GREET_ME'
+})
+
+console.log(store.getState())
