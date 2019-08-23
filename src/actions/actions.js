a6798973027a7b1b4097f67b7e5b851dffd7478b
Jemila <jemila.abulhawa@gmail.com>
2019-08-23 20:44:19 +0300

    add action type constants #13

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,3 +1,5 @@
+export const TOGGLE_MESSAGE = 'TOGGLE_MESSAGE'
+
 export const toggleMessage = () => {
   return {
     type: 'TOGGLE_MESSAGE'
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,10 +1,12 @@
+import { TOGGLE_MESSAGE } from '../actions/actions'
+
 const initialState = {
   messageVisibility: false
 }
 
 export default function(state = initialState, { type }) {
   switch (type) {
-    case 'TOGGLE_MESSAGE':
+    case TOGGLE_MESSAGE:
       return { ...state, messageVisibility: !state.messageVisibility }
     default:
       return state
